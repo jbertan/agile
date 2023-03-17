3bfd54d080f674275fd77c8b50587c19b1730719
jbertan <bertanakarsu@gmail.com>
2023-03-17 13:32:04 +0300

    pagination added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,22 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
-import RadarComponent from "@/components/radar";
 import StatementComponent from "@/components/statement";
 import MailComponent from "@/components/mail";
 import { datas, _categories } from "@/helper/database";
 import { useState, useReducer } from "react";
 import { reducerFunctions, initialReducers } from "@/helper/reducer";
 import RadarContainer from "@/components/radarcontainer";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [result, setResult] = useState<boolean>(false);
   const [mail, setMail] = useState<string>("");
   const [tasks, dispatch] = useReducer(reducerFunctions, initialReducers);
+  const [page, setPage] = useState<number>(1);
+
   const [resultArray, setResultArray] = useState<
     {
       categories: _categories;
@@ -31,6 +35,28 @@ export default function Home() {
   const submitHandler = (e: string) => {
     setMail(e);
   };
+  const renderItems = () => {
+    const pageByItems = 5;
+    const renderItems = [];
+    const start = pageByItems * (page - 1);
+    const end = start + pageByItems;
+    const items = datas.map((data, i) => (
+      <StatementComponent
+        key={data.id}
+        statement={data.statement}
+        setResultArray={setResultArray}
+        indexArray={i}
+        resultArray={resultArray}
+        categories={data.categories}
+        dispatch={dispatch}
+        tasks={tasks}
+      />
+    ));
+    for (let i = start; i < end; i++) {
+      renderItems.push(items[i]);
+    }
+    return renderItems;
+  };
   console.log(tasks);
   return (
     <>
@@ -47,24 +73,21 @@ export default function Home() {
         ) : mail ? (
           <>
             <h1 className="header">Agile Survey</h1>
-            {/* <div className="radarsize">
-          <RadarComponent />
-        </div> */}
-            {datas.map((data, i) => (
-              <StatementComponent
-                key={data.id}
-                statement={data.statement}
-                setResultArray={setResultArray}
-                indexArray={i}
-                resultArray={resultArray}
-                categories={data.categories}
-                dispatch={dispatch}
-                tasks={tasks}
-              />
-            ))}
-            <button onClick={resultHandler} className="button right">
-              Result &rArr;
-            </button>
+            {renderItems()}
+            {page === 7 && (
+              <button onClick={resultHandler} className="button right">
+                Result &rArr;
+              </button>
+            )}
+
+            <Pagination
+              className="top-margin"
+              count={Math.ceil(35 / 5)}
+              page={page}
+              color="primary"
+              onChange={(event, value) => setPage(value)}
+              variant="outlined"
+            />
           </>
         ) : (
           <MailComponent setMail={submitHandler} dispatch={dispatch} />
