0378fa96ea90adcdf768af64c8865428d0f6bf62
jbertan <bertanakarsu@gmail.com>
2023-03-30 11:31:05 +0300

    corrected

--- src/components/screenresolution.tsx
+++ src/components/screenresolution.tsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 const ScreenResolution = () => {
-  const [resolution, setResolution] = useState({
-    width: typeof window !== "undefined" ? window.innerWidth : 0,
-    height: typeof window !== "undefined" ? window.innerHeight : 0,
-  });
+  const [resolution, setResolution] = useState({ width: 0, height: 0 });
 
   useEffect(() => {
     if (typeof window === "undefined") {
@@ -15,6 +12,10 @@ const ScreenResolution = () => {
       setResolution({ width: window.innerWidth, height: window.innerHeight });
     };
 
+    // Set the initial resolution
+    updateResolution();
+
+    // Update the resolution when the window is resized
     window.addEventListener("resize", updateResolution);
 
     return () => {
